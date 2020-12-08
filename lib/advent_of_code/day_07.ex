20855710a836f39d3b0fc5940c9fc2f626d3290e
Tobias Kloht <tobias.kloht@klarna.com>
2020-12-08 23:36:26 +0100

    day 7 part 2

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -15,9 +15,9 @@ defmodule AdventOfCode.Day07 do
        if (Enum.member?(candidates, "no other")) do
         false
       else
-        IO.inspect(candidates, label: "Maybe Candidates")
+        # IO.inspect(candidates, label: "Maybe Candidates")
         new_candidates = get_new_candidates(rules, candidates)
-        IO.inspect(new_candidates, label: "new candidates")
+        # IO.inspect(new_candidates, label: "new candidates")
         Enum.map(new_candidates, fn x -> build_path(rules, x)end)
         |> Enum.reduce(fn x, y -> x || y end)
       end
@@ -27,6 +27,19 @@ defmodule AdventOfCode.Day07 do
   def normalize_bags(bag) do
     Regex.replace(~r/^\d+ /, bag, "")
     |> (fn x -> Regex.replace(~r/ bags?$/, x, "") end).()
+  end
+
+  def normalize_bags_2(bag) do
+    without_bags = Regex.replace(~r/ bags?$/, bag, "")
+    if (without_bags == "no other") do
+      []
+    else
+      [count, color] = Regex.run(~r/(\d+) (.+)/, without_bags, capture: :all_but_first)
+      # IO.inspect(count, label: "captures")
+      Enum.to_list( 1..String.to_integer(count))
+      |> Enum.map(fn _ -> color end)
+    end
+
   end
 
   def part1(args) do
@@ -43,6 +56,27 @@ defmodule AdventOfCode.Day07 do
     |> Enum.count()
   end
 
-  def part2(_args) do
+  def collapse_bags(current_rule, all_rules) do
+    {_color, bags} = current_rule
+
+    bags
+    |> Enum.concat(Enum.flat_map(bags, fn search_color ->
+      new_rule = Enum.find(all_rules, fn({color, _bags}) -> color == search_color end)
+      collapse_bags(new_rule, all_rules)
+    end))
+  end
+
+  def part2(args) do
+    rules = args
+    |> (fn x -> Regex.replace(~r/\.$/, x, "") end).()
+    |> String.split(".\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(fn x -> String.split(x, " bags contain ") end)
+    |> Enum.map(fn [left, right] -> {left, right |> String.split(", ") |> Enum.flat_map(&normalize_bags_2/1)} end)
+
+    # IO.inspect(rules, label: "rules")
+
+    golden_rule = Enum.find(rules, fn({color, _bags}) -> color == "shiny gold" end)
+    Enum.count(collapse_bags(golden_rule, rules))
   end
 end
--- test/advent_of_code/day_07_test.exs
+++ test/advent_of_code/day_07_test.exs
@@ -18,11 +18,16 @@ defmodule AdventOfCode.Day07Test do
     assert result == 4
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "shiny gold bags contain 2 dark red bags.
+    dark red bags contain 2 dark orange bags.
+    dark orange bags contain 2 dark yellow bags.
+    dark yellow bags contain 2 dark green bags.
+    dark green bags contain 2 dark blue bags.
+    dark blue bags contain 2 dark violet bags.
+    dark violet bags contain no other bags."
     result = part2(input)
 
-    assert result
+    assert result == 126
   end
 end
